1f8097535778a15a118a92cb4b2a37a784acdfd0
sarvagnavemula7 <150222544+sarvagnavemula7@users.noreply.github.com>
2025-07-09 12:21:46 +0530

    program to print fullname and age+5

---
+++ Program1.py
@@ -0,0 +1,8 @@
+#Write a Python program that asks the user to enter their full name and age.
+# The program should then print:
+#"Hello, <NAME>! You will be <AGE + 5> years old in 5 years."
+
+fullname=input("Enter your FullName: ")
+age=int(input("Enter your Age: "))
+print("Hello, "+fullname+"! You will be",age+5,"years old in 5 years.")
+
